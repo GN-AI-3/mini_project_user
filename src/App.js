a627e09a0a0e08c35e665eff9b27b0b5dc1c1990
RJANG <150069439+R-JANG-98@users.noreply.github.com>
2025-03-14 16:23:28 +0900

    현재까지

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
+import DrawingApp from "./pages/DrawingApp";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/result" element={<Result />} />
+          <Route path="/draw" element={<DrawingApp />} />
         </Routes>
       </div>
     </Router>
