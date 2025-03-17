d2c2b886583a43d3028550cf3859b0313b2acee0
JIHYE <jihyeoh91@gmail.com>
2025-03-17 16:58:51 +0900

    칠판 위 이미지 적용

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import DrawingApp from "./pages/DrawingApp";
 import Home from "./pages/Home";
 import Result from "./pages/Result";
 import { ToggleProvider, useToggle } from "./ToggleContext";
+import blackboardEraser from "./images/blackboardEraser.png"; // 이미지 import
+import BlackboardGraffiti from "./images/Blackboard-graffiti.png"; // 이미지 import
 
 function App() {
   return (
@@ -17,6 +19,11 @@ function AppContent() {
 
   return (
     <div className="App">
+      <p className="noisy-person-text">떠든 사람 : ㅁㅁㅁ</p>
+      <p className="pranksters-person-text">장난친 사람 : ㅁㅁㅁ</p>
+      <img className="BlackboardGraffiti-img" src={BlackboardGraffiti} alt="BlackboardGraffiti" />
+      <img className="blackboardEraser-img" src={blackboardEraser} alt="blackboardEraser" />
+
       <div className="drawing-background">
         <DrawingApp />
       </div>
